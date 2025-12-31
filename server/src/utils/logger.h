eae465c831c50dfa1fb45ca7160034fe230cc30d
anybody1234 <167839131+anybody1234@users.noreply.github.com>
2025-12-31 16:52:27 +0700

    fix

--- server/src/utils/logger.h
+++ server/src/utils/logger.h
@@ -1,16 +1,8 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-// Simple project-wide activity log.
-// Each line format:
-// [dd/mm/yyyy HH:MM:SS]$<ip>$<request>$<result>
-// Example:
-// [20/12/2025 15:00:11]$127.0.0.1$LOGIN|huyen|1234$200|Login successful
-
 void protocol_set_current_request_for_log(const char *request_line);
 void protocol_clear_current_request_for_log(void);
-
-// Log a result string for the current request set via protocol_set_current_request_for_log.
 void protocol_log_result(int client_sock, const char *result);
 
 #endif
